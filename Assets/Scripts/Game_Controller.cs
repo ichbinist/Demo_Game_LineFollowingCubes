54fe2de4fcfc2d69155477918618f1e79311e08a
Oğuzhan Arar <ichbinizm@gmail.com>
2020-04-19 19:57:31 +0300

    Removing Lines
    Removing lines when object is moving, Scene sending log when scene is finished.

---
+++ Assets/Scripts/Game_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Controller : MonoBehaviour
+{
+  private GameObject[] cubes;
+  private int finishcount = 0;
+    void Start(){
+    cubes = GameObject.FindGameObjectsWithTag("cube");
+    }
+
+    void Update()
+    {
+        foreach(GameObject cube in cubes){
+          if(cube.GetComponent<Line_Render>().isfinished == true && cube.GetComponent<Line_Render>().isadded == false){
+            finishcount++;
+            cube.GetComponent<Line_Render>().isadded = true;
+          }
+        }
+        if(finishcount >= cubes.Length){
+          Debug.Log("Scene Finished");
+          gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Line_Render.cs
+++ Assets/Scripts/Line_Render.cs
@@ -8,7 +8,7 @@ public class Line_Render : MonoBehaviour
     //Publics
     public GameObject linePoint;
     public GameObject finishingPoint;
-    public float speed = 4f;
+    public float speed = 6f;
     [Header("Add New Points to Position")]
     public Vector3 position = new Vector3(0,0,0);
     [Header("Points to Draw Line and to follow")]
@@ -16,12 +16,14 @@ public class Line_Render : MonoBehaviour
     //Privates
     [SerializeField]
     private LineRenderer linerender;
-    private bool added = false;
     private int i = 1;
     private bool movestart = false;
-
+    public bool collision = false;
+    public bool isfinished = false;
+    public bool isadded = false;
     void Start()
     {
+        linerender = GetComponent<LineRenderer>();
         linerender.SetWidth(0.2f,0.2f);
     }
 
@@ -30,20 +32,21 @@ public class Line_Render : MonoBehaviour
     }
 
     public void addPoint(){
-      added = true;
+      linerender = GetComponent<LineRenderer>();
       GameObject Addedobject = Instantiate(linePoint, transform.position+position, Quaternion.identity);
-      Addedobject.transform.parent = transform;
+      //Addedobject.transform.parent = transform;
       linePoints.Add(Addedobject);
     }
 
     public void addEnd(){
-      added = true;
+
       GameObject Addedobject = Instantiate(finishingPoint, transform.position+position, Quaternion.identity);
-      Addedobject.transform.parent = transform;
+      //Addedobject.transform.parent = transform;
       linePoints.Add(Addedobject);
     }
-    
+
     public void PointsToLine(){
+
       if(gameObject.GetComponent<LineRenderer>() == null){
         linerender = gameObject.AddComponent<LineRenderer>();
         linerender.SetVertexCount(linePoints.Count);
@@ -51,12 +54,13 @@ public class Line_Render : MonoBehaviour
          linerender.SetPosition(i, linePoints[i].transform.position);
         }
       }else{
-          linerender = GetComponent<LineRenderer>();
+      linerender = GetComponent<LineRenderer>();
       linerender.SetVertexCount(linePoints.Count);
       for (int i = 0; i < linePoints.Count; i++){
        linerender.SetPosition(i, linePoints[i].transform.position);
       }
     }
+
     }
 
     void OnMouseDown(){
@@ -68,16 +72,43 @@ public class Line_Render : MonoBehaviour
     }
 
     public void resetPoints(){
-      foreach(GameObject linePoint in linePoints){DestroyImmediate(linePoint);}
-      linerender.SetVertexCount(0);
+      foreach(GameObject linePoint in linePoints){if(linePoint != linePoints[0])DestroyImmediate(linePoint);}
+      GameObject temp = linePoints[0];
+      linerender.SetVertexCount(1);
       linePoints.Clear();
+      linePoints.Add(temp);
     }
 
     void FixedUpdate()
     {
       if(transform.position == linerender.GetPosition(i) && i<linePoints.Count){i++;}
+      if(transform.position == linerender.GetPosition(linePoints.Count)){isfinished = true;}
       if(movestart == true){
       transform.position = Vector3.MoveTowards(transform.position, linerender.GetPosition(i), speed * Time.deltaTime);
+
+      for (int j = 0; j < linePoints.Count; j++){
+       linerender.SetPosition(j, linePoints[j].transform.position);
+      }
+
       }
     }
+
+    void OnTriggerEnter(Collider other)
+   {
+     Debug.Log("Collision");
+      if (other.CompareTag("cube")){
+        Application.LoadLevel(Application.loadedLevel);
+        Debug.Log("Collision");
+      }
+   }
+
+   void OnTriggerStay(Collider other){
+     bool colTrigger = false;
+     if (other.CompareTag("point")){
+       foreach(GameObject linePoint in linePoints){if(other.gameObject == linePoint){colTrigger = true;}}
+       if(colTrigger == true && transform.position == other.gameObject.transform.position){
+       other.transform.parent = this.transform;
+       other.gameObject.transform.position = transform.position;}
+     }
+   }
 }
