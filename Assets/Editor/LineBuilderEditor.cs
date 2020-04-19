1ff0a462c722766761f4a3dcf84bfb3426e4ab6c
Oğuzhan Arar <ichbinizm@gmail.com>
2020-04-19 14:12:27 +0300

    Custom Inspector Added
    Created a custom inspector to add points to our Points list and clear our list.

---
+++ Assets/Editor/LineBuilderEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Line_Render))]
+public class LineBuilderEditor : Editor
+{
+  public override void OnInspectorGUI()
+      {
+          DrawDefaultInspector();
+
+          Line_Render script = (Line_Render)target;
+          if(GUILayout.Button("Add Point"))
+          {
+              script.addPoint();
+          }
+          if(GUILayout.Button("Reset Points"))
+          {
+              script.resetPoints();
+          }
+
+      }
+}
---
+++ Assets/Scripts/Line_Render.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Line_Render : MonoBehaviour
+{
+    //Publics
+    public GameObject linePoint;
+    public GameObject finishingPoint;
+    [Header("Add New Points to Position")]
+    public Vector3 position = new Vector3(0,0,0);
+    [Header("Points to Draw Line and to follow")]
+    public List<GameObject> linePoints;
+    //Privates
+    [SerializeField]
+    private LineRenderer linerender;
+    private bool added = false;
+
+    void Start()
+    {
+      linerender = gameObject.AddComponent<LineRenderer>();
+    }
+
+    void setLinePoints(){
+      foreach (Transform child in gameObject.transform){if(child.CompareTag("point")){linePoints.Add(child.gameObject);} }
+    }
+
+    public void addPoint(){
+      added = true;
+      GameObject Addedobject = Instantiate(linePoint, position, Quaternion.identity);
+      Addedobject.transform.parent = transform;
+      linePoints.Add(Addedobject);
+    }
+
+    public void resetPoints(){
+      foreach(GameObject linePoint in linePoints){DestroyImmediate(linePoint);}
+      linePoints.Clear();
+    }
+    void Update()
+    {
+
+    }
+}
